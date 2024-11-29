1c28c352fff92cc01df80a0a4ce6d81b8563500f
kfiddle <kenjfiddle@gmail.com>
2024-11-28 21:58:52 -0500

    disable and obscure buttons if game clock isn't running

--- components/playerStats/statButton/StatCell.jsx
+++ components/playerStats/statButton/StatCell.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GameContext } from '../../contextProviders/GameContext.jsx';
 import Disc from '../../disc/Disc.jsx';
 
 import styles from './StatCell.module.css';
+
 const StatCell = ({ stat, value, isActive, hasDisc, onTouchStart, onTouchEnd, menuOpen, onMenuAction, onCloseMenu }) => {
+  const { gameState } = useContext(GameContext);
+  const { isClockRunning } = gameState;
+
   const isErrorStat = ['drop', 'throw', 'stall'].includes(stat);
-  const isDisabled = !hasDisc || (isErrorStat && !hasDisc);
+  const isDisabled = !isClockRunning || !hasDisc || (isErrorStat && !hasDisc);
 
   return (
     <td
@@ -16,23 +21,24 @@ const StatCell = ({ stat, value, isActive, hasDisc, onTouchStart, onTouchEnd, me
       <button
         className={`
                     ${styles.statButton}
+                    ${!isClockRunning ? styles.clockStoppedButton : ''}
                     ${!hasDisc ? styles.inactiveStatButton : ''}
-                    ${stat === 'hasDisc' ? styles.hasDiscButton : ''}
+                    ${stat === 'hasDisc' && isClockRunning && hasDisc ? styles.hasDiscButton : ''}
                     ${stat === 'D' ? styles.dButton : ''}
                     ${isErrorStat ? styles.errorButton : ''}
                     ${isDisabled ? styles.disabledErrorButton : ''}
                 `}
-        onTouchStart={(e) => onTouchStart(stat, e)}
-        onTouchEnd={(e) => onTouchEnd(stat, e)}
+        onTouchStart={(e) => isClockRunning && onTouchStart(stat, e)}
+        onTouchEnd={(e) => isClockRunning && onTouchEnd(stat, e)}
         disabled={isDisabled}
       >
-        {stat === 'hasDisc' && hasDisc? (
+        {stat === 'hasDisc' && hasDisc && isClockRunning ? (
           <Disc className={styles.discIcon} />
         ) : (
           <span className={styles.statLabel}>{stat === 'hasDisc' ? '' : stat}</span>
         )}
       </button>
-      {menuOpen && (
+      {menuOpen && isClockRunning && (
         <div className={styles.longPressMenu}>
           <button onTouchEnd={() => onMenuAction(stat, false)}>Decrement</button>
           <button onTouchEnd={() => onMenuAction(stat, true)}>Increment</button>
